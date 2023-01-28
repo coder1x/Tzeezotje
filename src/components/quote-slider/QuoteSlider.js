d3420dbae649b3ca3242b5aeff0034d59d372df8
Kliushin Anton <c.o.d.e.r.1@mail.ru>
2023-01-28 19:11:52 +0500

    3 секция готова

--- src/components/quote-slider/QuoteSlider.js
+++ src/components/quote-slider/QuoteSlider.js
@@ -7,11 +7,8 @@ class QuoteSlider {
   }
 
   static initSwiper() {
-    new Swiper('.js-quote-slider', {
+    new Swiper('.js-quote-slider__wrapper', {
       modules: [Navigation],
-      slidesPerView: 1,
-      wrapperClass: 'quote-slider__items',
-      slideClass: 'quote-slider__item',
       loop: 'auto',
       navigation: {
         nextEl: '.js-quote-slider__next',
